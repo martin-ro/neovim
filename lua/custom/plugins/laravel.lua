519ca53641205b2aa83cdc32f8c7c1fa97210b1b
Martin Ro <martin-ro@users.noreply.github.com>
2024-12-11 04:26:09 +0800

    Initial commit

---
+++ lua/custom/plugins/laravel.lua
@@ -0,0 +1,21 @@
+-- Plugin for Neovim to enhance the development experience of Laravel projects
+
+-- https://github.com/adalessa/laravel.nvim
+
+return {
+    "adalessa/laravel.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "tpope/vim-dotenv",
+        "MunifTanjim/nui.nvim",
+        "nvimtools/none-ls.nvim",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+        { "<leader>la", ":Laravel artisan<cr>" },
+        { "<leader>lr", ":Laravel routes<cr>" },
+        { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    config = true,
+}
---
+++ lua/custom/plugins/namespace.lua
@@ -0,0 +1,16 @@
+-- Neovim Php Namespace Resolver
+
+-- https://github.com/ccaglak/namespace.nvim
+
+return {
+    'ccaglak/namespace.nvim',
+    keys = {
+        { "<leader>la", "<cmd>GetClasses<cr>"},
+        { "<leader>lc", "<cmd>GetClass<cr>"},
+        { "<leader>ls", "<cmd>ClassAs<cr>"},
+        { "<leader>ln", "<cmd>Namespace<cr>"},
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    }
+}
